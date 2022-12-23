d7a4f17d3e6c2e682a01f3385d5404499080394a
Fortuna <58559930+Fortuna123@users.noreply.github.com>
2022-12-23 23:14:34 +0100

    Create OrderController.cs

---
+++ OnlineShopProject/Areas/Customer/Controllers/OrderController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using OnlineShopProject.Models;
+using OnlineShopProject.Data;
+using OnlineShopProject.Utility;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopProject.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET Checkout Action Method
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        //POST Checkout Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Checkout(Order anOrder)
+        {
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+            if (products != null)
+            {
+                foreach (var product in products)
+                {
+                    OrderDetails orderDetails = new OrderDetails();
+                    orderDetails.ProductId = product.Id;
+                    anOrder.OrderDetails.Add(orderDetails);
+                }
+            }
+
+            anOrder.OrderNo = GetOrderNo();
+            _db.Orders.Add(anOrder);
+            await _db.SaveChangesAsync();
+            HttpContext.Session.Set("products", new List<Products>());
+
+            return View();
+        }
+
+        public string GetOrderNo()
+        {
+            int rowCount = _db.Orders.ToList().Count() + 1;
+            return rowCount.ToString("000");
+        }
+    }
+}
